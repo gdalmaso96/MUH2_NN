01fb8ef0ccab68f077da559664f163316e82e312
gdalmaso96 <g.dalmaso@outlook.it>
2022-09-05 16:40:49 +0200

    v 1

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 This surrogate takes as an input a database and models only the delivered intensity and the central momentum.
 
 Will try to optimize Rebecca's version of MUH2 with reasonable current limits and compare to normal optimization.
+
+
--- Surrogate_00003.py
+++ Surrogate_00003.py
@@ -8,11 +8,15 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import set_random_seed
 from tensorflow.keras.models import load_model
+from sklearn.model_selection import KFold
 from multiprocessing.pool import ThreadPool
 import subprocess
 
 np.random.seed(299792458)
 set_random_seed(299792458)
+
+# Added kfold cross validation for training and testing
+
 
 ######### Function DBtonumpy: extracts DB data to be saved in npy files
 def DBtonumpy(studyname, database, objective, normalization):
@@ -144,6 +148,96 @@ def train(studyname, database, objective, batchsize, normalization, nnodes, dept
 
     
     return results
+    
+    
+######### Function trainKfold: as an input it takes the objective index, the DB name and num_folds. It saves the resulting model and plots the learning results
+def trainKfold(studyname, database, objective, batchsize, normalization, nnodes, depth, epochs, num_folds):
+    # Load data
+    X = np.load('X.npy')
+    Y = np.load('Y.npy')
+    
+    # Prepare target
+    Y_tgt = []
+    
+    for o in objective:
+        Y_tgt.append(Y[:, o])
+    
+    Y = np.array(Y_tgt)
+    
+    # Create KFold
+    kfold = KFold(n_splits=num_folds, shuffle=True)
+    
+    # Train
+    no_fold = 1
+    loss_per_fold = []
+    for train, test in kfold.split(X, Y):
+        # Create model
+        model = Sequential()
+        model.add(Dense(nnodes, input_shape=(len(list(X[0])), ), activation='tanh'))
+        for i in range(depth - 1):
+            model.add(Dense(nnodes, activation='tanh'))
+        
+        model.add(Dense(len(objective)))
+        optimiser = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
+        model.compile(optimizer=optimiser, loss='mse')
+        
+        # Generate a print
+        print('------------------------------------------------------------------------')
+        print(f'Training for fold {fold_no} ...')
+        history = model.fit(X[train], Y[train],
+              batch_size=batchsize,
+              epochs=epochs,
+              verbose=verbosity)
+    
+    
+        # Generate generalization metrics
+        scores = model.evaluate(X[test], Y[test], verbose=0)
+        loss_per_fold.append(scores[0])
+
+        # Increase fold number
+        model.save(studyname + '_%03dneurons_%03dlayers_%04depochs_obj%d_%dbatch_%dfold_save' %(nnodes, depth, epochs, len(objective), batchsize, fold_no))
+        fold_no = fold_no + 1
+        
+        
+    # == Provide average scores ==
+    print('------------------------------------------------------------------------')
+    print('Score per fold')
+    for i in range(0, len(loss_per_fold)):
+        print('------------------------------------------------------------------------')
+        print(f'> Fold {i+1} - Loss: {loss_per_fold[i]}')
+    print('------------------------------------------------------------------------')
+    print('Average scores for all folds:')
+    print(f'> Loss: {np.mean(loss_per_fold)}')
+    print('------------------------------------------------------------------------')
+    
+    # Load best modelnumpy.argmin
+    model = load_model(studyname + '_%03dneurons_%03dlayers_%04depochs_obj%d_%dbatch_%dfold_save' %(nnodes, depth, epochs, len(objective), batchsize, np.argmin(np.array(loss_per_fold))))
+    
+    
+    for i in range(len(objective)):
+        Y_pred = model.predict(X)
+        plt.plot(Y_pred[:, i], Y[:, i], '.', label=r'$\rho$ = %.6f' %(np.corrcoef(Y_pred[:, i], Y[:, i])[0][1]))
+        if(objective[i] == 0):
+            plt.plot([0, 0.2], [0, 0.2], '--', color = 'r')
+        else:
+            plt.plot([0, 1], [0, 1], '--', color = 'r')
+        
+        plt.title('Prediction on train, objective %d' %(objective[i]))
+        plt.ylabel('Train value')
+        plt.xlabel('Prediction')
+        plt.legend(loc='upper left')
+        #plt.show()
+        plt.savefig('fig/' + studyname + '_%03dneurons_%03dlayers_%04depochs_%dobj%d_%dbatch_%dfold_test.png' %(nnodes, depth, epochs, objective[i], len(objective), batchsize, np.argmin(np.array(loss_per_fold))))
+        plt.clf()
+        
+        corr.append(np.corrcoef(Y_pred[:, i], Y[:, i])[0][1])
+    
+    
+    
+    
+    
+
+# Test with 10 epochs on 10 folds 50 batch
 
 if __name__ == '__main__':
     studyname = 'MUH2_V5_Peter_TOT_mup_mono'
@@ -156,7 +250,7 @@ if __name__ == '__main__':
     objective.append(3)
     objective.append(4)
     '''
-    batchsize_s = 100
+    batchsize_s = 50
     normalization = []
     normalization.append((2922072/2e11*2.4e-3/1.6e-19)*0.2) # 20% of total muons
     '''
@@ -167,15 +261,16 @@ if __name__ == '__main__':
     '''
     nnodes_s = 20
     depth_s = 1
-    epochs = 10000
+    epochs = 10
     start_time = time.time()
     #DBtonumpy(studyname, database, objective, normalization)
-    for i in range(20):
-        for k in range(10):
+    num_folds = 10
+    for i in range(3, 4): # iterate on NN depth
+        for k in range(0, 1): # iterate on batch size
             depth = depth_s + i
             nnodes = nnodes_s
-            batchsize = batchsize_s + k*100
-            train(studyname, database, objective, batchsize, normalization, nnodes, depth, epochs) # 1 layer
+            batchsize = batchsize_s + k*50
+            trainKfold(studyname, database, objective, batchsize, normalization, nnodes, depth, epochs, num_folds) # 1 layer
             print(time.time() - start_time)
             
         
