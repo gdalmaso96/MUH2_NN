d69403733a5af81345c26f4b786d3583b8d5a5bb
gdalmaso96 <g.dalmaso@outlook.it>
2022-09-17 10:57:27 +0200

    v 1

--- Surrogate_00003.py
+++ Surrogate_00003.py
@@ -178,7 +178,7 @@ def trainKfold(studyname, database, objective, batchsize, normalization, nnodes,
         print(train.shape)
         print('------------------------------------------------------------------------')
         print(f'Training for fold {fold_no} ...')
-        history = model.fit(X[train], Y[train, objective],
+        history = model.fit(X[train], Y[train, objective[np.newaxis,:]],
               batch_size=batchsize,
               epochs=epochs)
     
@@ -194,7 +194,7 @@ def trainKfold(studyname, database, objective, batchsize, normalization, nnodes,
         plt.savefig('fig/' + studyname + '_%03dneurons_%03dlayers_%04depochs_obj%d_%dbatch_%dfold%d_loss.png' %(nnodes, depth, epochs, len(objective), batchsize, fold_no, num_folds))
         plt.clf()    
         # Generate generalization metrics
-        scores = model.evaluate(X[test], Y[test, objective[np.newaxis,:]],)
+        scores = model.evaluate(X[test], Y[test, objective],)
         loss_per_fold.append(scores)
 
         # Increase fold number
